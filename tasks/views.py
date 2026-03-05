24c16a7feb5c5d3bb608df7fd68dd486b2479c95
Arooba Arshad <arobaarshad@gmail.com>
2026-03-05 12:35:19 +0100

    Add user login and logout using Django auth views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    tasks = Task.objects.filter(assignee=request.user)
+
+    context = {
+        "tasks": tasks
+    }
+
+    return render(request, "tasks/dashboard.html", context)
